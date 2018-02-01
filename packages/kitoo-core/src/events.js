ec284a740780489458401c486992a406331485f0
David Harutyunyan <davidhn96@yahoo.com>
2018-02-01 18:06:19 +0400

    fixing events after merge

--- packages/kitoo-core/src/events.js
+++ packages/kitoo-core/src/events.js
@@ -1,6 +1,8 @@
 const KitooCoreEvents = {
   ROUTER_FAIL: 'router.fail',
   ROUTER_STOP: 'router.stop',
+  ROUTER_RECONNECT: 'router.reconnect',
+  ROUTER_RECONNECT_FAILURE: 'router.reconnect.failure',
   NEW_ROUTER: 'new.router',
   SERVICE_WELCOME: 'service.welcome',
   SERVICE_FAIL: 'service.fail',
